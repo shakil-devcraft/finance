4946f276b549b042d4c300d004b959f29bec15c7
Shakil Ahmed <shakil.devcraft@gmail.com>
2024-11-22 15:09:36 +0600

    we offer section now completed

--- src/pages/Home/WeOffer.jsx
+++ src/pages/Home/WeOffer.jsx
@@ -1,12 +1,23 @@
+import { weOffer } from "../../api";
 import Container from "../../components/shares/Container/Container";
 
 const WeOffer = () => {
     return (
-        <section>
+        <section className="py-10 md:py-16 lg:py-32">
             <Container>
-                <h1 className="text-2xl md:text-3xl lg:text-[40px] leading-[30px] md:leading-[57px] text-center text-white font-bold py-10 md:py-16 lg:py-32">What do we offer?</h1>
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-5 md:gap-6 lg:gap-8">
-                    
+                <h1 className="lg:mb-16 mb-10 text-2xl md:text-3xl lg:text-[40px] leading-[30px] md:leading-[57px] text-center text-white font-bold">What do we offer?</h1>
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-5 md:gap-6 lg:gap-8 space-y-8 md:space-y-0">
+                    {
+                        weOffer?.map(({offerImg, title, description}, offerIdx) => (
+                            <div className="flex flex-row md:flex-col lg:flex-row gap-4 md:gap-5 lg:gap-6" key={offerIdx}>
+                                <img className="size-16" src={offerImg} alt="" />
+                                <div>
+                                    <h3 className="font-medium text-white lg:text-2xl text-lg mb-2">{title}</h3>
+                                    <p className="font-normal lg:text-base text-sm text-[#ADB2B1] lg:w-[280px]">{description}</p>
+                                </div>
+                            </div>
+                        ))
+                    }
                 </div>
             </Container>
         </section>
